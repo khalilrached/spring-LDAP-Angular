dc379c7c98f2e62d6ff726dfef1fc732d77b8157
khalilrached <khalilrached058@gmail.com>
2022-08-22 14:09:54 +0100

    init file handling by initializing uploads folder

--- back-end/src/main/java/com/leoni/stage/StageApplication.java
+++ back-end/src/main/java/com/leoni/stage/StageApplication.java
@@ -1,17 +1,30 @@
 package com.leoni.stage;
 
+import com.leoni.stage.services.FileStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
+
 
 @SpringBootApplication
 @Controller
-public class StageApplication {
+public class StageApplication implements CommandLineRunner {
+
+	@Resource
+	FileStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StageApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
 	}
 
 	@RequestMapping("/")
