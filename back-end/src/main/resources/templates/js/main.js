ed0943c5b5637ed01b3a768dd180675d42e405b0
khalilrached <khalilrached058@gmail.com>
2022-08-23 16:11:58 +0100

    adding fetchData function to fetch the autocomplete values from server

--- back-end/src/main/resources/templates/js/main.js
+++ back-end/src/main/resources/templates/js/main.js
@@ -11,3 +11,24 @@ btn.addEventListener('click',()=>{
         dropMenu.classList.add('active');
     }
 })
+
+function fetchData(){
+    console.log("we are in fetch func !");
+    const text = document.getElementById('text');
+    const dataList = document.getElementById('data');
+    dataList.innerHTML = "";
+    const bodydata = {
+        "searchToken":text.value
+    }
+    console.log('data value:' , text.value);
+    fetch('http://localhost:8080/form/autoCompleteData/'+text.value)
+    .then((response)=>response.json())
+    .then((data)=>{
+       data.forEach(element => {
+            dataList.innerHTML += `<option vlaue="${element}"> ${element} </option>`
+       }); 
+    })
+    .catch((err)=>{
+        console.error(err);
+    })    
+}
